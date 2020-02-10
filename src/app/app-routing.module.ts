f8a71116fec5ad5c7cebedef81374875ed2d679e
will <navyhockey56@gmail.com>
2020-02-10 18:04:45 -0500

    Inital creation of willdengler.com as an angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppHomeComponent } from './_components/app-home/app-home.component'
+import { ContactComponent } from './_components/contact/contact.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppHomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './_components/article/article.component';
+import { AppHomeComponent } from './_components/app-home/app-home.component';
+import { AppNavComponent } from './_components/app-nav/app-nav.component';
+import { ContactComponent } from './_components/contact/contact.component';
+import { ResumeComponent } from './_components/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    AppHomeComponent,
+    AppNavComponent,
+    ContactComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
