a152d3b88a4e45f3971bebb3dc8a6a9b73cfa57e
will <navyhockey56@gmail.com>
2020-02-11 21:26:59 -0500

    Basic updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppHomeComponent } from './_components/app-home/app-home.component'
 import { ContactComponent } from './_components/contact/contact.component'
+import { ResumeComponent } from './_components/resume/resume.component'
 
 const routes: Routes = [
   { path: '', component: AppHomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'resume', component: ResumeComponent },
 ];
 
 @NgModule({
