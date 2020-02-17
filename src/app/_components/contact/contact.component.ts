7229c35b2599e249240e7161922dd7a72a840f96
will <navyhockey56@gmail.com>
2020-02-17 08:11:42 -0500

    Updating contact page to have success/error popups for message creation. Updating service-helpers#handleError to allow a error handler operation to be passed in to consume the generated error.

--- src/app/_components/contact/contact.component.ts
+++ src/app/_components/contact/contact.component.ts
@@ -10,7 +10,20 @@ import { MessageService } from '../../_services/message.service';
 })
 export class ContactComponent implements OnInit {
 
-  @Input() message;
+  /*
+    TODOS
+    - Add email validation
+    - Add email icon
+  */
+
+  // @Input() email;
+
+  // Indicates that message creation succeeded
+  successMessage: boolean = false;
+  // Indicates that we failed to create a message
+  failedMessage: boolean = false;
+  // Stores the error message to be displayed when message creation fails
+  errorMessage: string;
 
   constructor(
     private messageService: MessageService
@@ -19,14 +32,76 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  saveMessage(email: string, body: string) {
-    console.log(email);
-    this.messageService.createMessage(mapToMessage({ email: email, message: body })).subscribe( message => {
-      if (!message) { return };
+  /**
+    Sends the message defined by the contact page to the server.
+    @param [string] email (Optional) - The email to respond to the message with
+    @param [string] body - The content of the message
+  */
+  saveMessage(email: string, body: string): void {
+    // Make sure there's a message body
+    if (!body || body.length <= 0) {
+      // Open failed message popup
+      this.errorMessage = "Looks like you forgot to enter your message";
+      this.failedMessage = true;
+      return;
+    }
 
-      console.log("Created message!");
-      console.log(message);
+    // TODO: Add email validation
+    // if (!this.email.validate()) {
+    //   this.failedMessage = true;
+    //   this.errorMessage = "Invalid email format";
+    //   return;
+    // }
+
+    // Create the message to be sent
+    let message = mapToMessage({ email: email, message: body });
+
+    // Create the message
+    this.messageService.createMessage(message, this.handleMessageError()).subscribe( message => {
+      if (!message) {
+        // Message creation failed - make sure error popup opens
+        this.failedMessage = true;
+        return;
+      }
+
+      // Message was created successfully - open success popup
+      this.successMessage = true;
     });;
+  }
+
+  /**
+    Removes the failed message popup from view
+  */
+  exitFailedMessage(): void {
+    this.failedMessage = false;
+  }
+
+  /**
+    Removes the success message popup from view
+  */
+  exitSuccessMessage(): void {
+    this.successMessage = false;
+  }
+
+  /**
+    Handles any errors from the messageService.
+  */
+  private handleMessageError() {
+    return (error: any) => {
+      if (!error || !error.status) {
+        // Unknown error
+        this.errorMessage = "An error occurred while creating your message.";
+      } else if (error.status == 406) {
+        // Invalid email or message too long
+        this.errorMessage = error.error.message;
+      } else {
+        // Unknown error
+        this.errorMessage = "An error occurred while creating your message.";
+      }
+
+      // Open failed message popup
+      this.failedMessage = true;
+    }
   }
 
 }
